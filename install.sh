b25fa6d5164b268eac7f0fb580caa2112c8b9e14
Daniel Krol <orblivion@gmail.com>
2015-01-31 22:45:36 -0800

    pipe != logical or

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 BASE_DIR=/var/debug_cron
 USER_DIR=$BASE_DIR/users
 
-mkdir $BASE_DIR | chown root:root $BASE_DIR
+mkdir $BASE_DIR || chown root:root $BASE_DIR
 chmod 777 $BASE_DIR
 
-mkdir $USER_DIR | chown root:root $USER_DIR
+mkdir $USER_DIR || chown root:root $USER_DIR
 chmod 777 $USER_DIR
